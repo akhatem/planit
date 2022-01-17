00522d1d1d474a927fe86e32f82e960b24964f10
Ahmed Kareem <ahmed.kareem.elshawadfy@gmail.com>
2022-01-17 19:54:37 +0200

    rename application to planitint

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,1 +1,3 @@
-server '169.254.95.120', user: 'deploy', roles: %w{app db web}
+server '62.114.120.114', user: 'deploy', roles: %w{app db web}, :primary => true
+
+set :deploy_to, "/home/deploy/planitint"
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -3,7 +3,7 @@ require "active_support/core_ext/integer/time"
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
-  # config.asset_host = 'https://www.planitint.com'
+  config.asset_host = 'https://www.planitintint.com'
 
   # In the development environment your application's code is reloaded any time
   # it changes. This slows down response time but is perfect for development
